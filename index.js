e26dd35009b47f41297af62a0e1793d391348456
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-19 18:20:43 -0700

    Completes arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// is an equation whose multiplied results will equal 62
+let num1 = 31, num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+// generates a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// is an equation that calculates a remainder that is equal to 4
+let num3 = 6, num4 = 2;
+
+let mod = num3 - num4;
+
+// will return 20 as the highest number in the set
+let max = Math.max(5,10,15,20);
